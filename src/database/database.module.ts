fc9386a4e43c705f4a51b17cb28ae0821e800889
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-11-10 15:07:38 +0700

    Create Dynamic Module - Improved

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,16 +1,19 @@
-import { Module } from '@nestjs/common';
-import {createConnection} from "typeorm";
+import {DynamicModule, Module} from '@nestjs/common';
+import {ConnectionOptions, createConnection} from "typeorm";
 
-@Module({
-    providers: [
-        {
-            provide: 'CONNECTION',
-            useValue: createConnection({
-                type: 'postgres',
-                host: 'localhost',
-                port: 5432,
-            }),
-        },
-    ],
-})
-export class DatabaseModule {}
+@Module({})
+
+// Improved Dynamic Module way of creating CONNECTION provider
+export class DatabaseModule {
+    static register(options: ConnectionOptions): DynamicModule {
+        return {
+            module: DatabaseModule,
+            providers: [
+                {
+                    provide: 'CONNECTION',
+                    useValue: createConnection(options),
+                }
+            ]
+        }
+    }
+}
