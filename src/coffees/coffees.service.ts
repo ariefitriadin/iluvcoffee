89b1792416b0ee65f30f4ae09ff42dae342f922a
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-11-13 01:40:48 +0700

    update config to use Inject

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -8,7 +8,8 @@ import {Flavor} from "./entities/flavor.entity";
 import {PaginationQueryDto} from "../common/dto/pagination-query.dto";
 import {Event} from "../events/entities/event.entity";
 import {COFFEE_BRANDS} from "./coffees.constants";
-import {ConfigService} from "@nestjs/config";
+import {ConfigService, ConfigType} from "@nestjs/config";
+import coffeesConfig from "./config/coffees.config";
 
 @Injectable()
 /*
@@ -25,9 +26,10 @@ export class CoffeesService {
         private readonly connection: Connection,
         @Inject(COFFEE_BRANDS) coffeeBrands: string[],
         private readonly configService: ConfigService,
+        @Inject(coffeesConfig.KEY)
+        private coffeesConfiguration: ConfigType<typeof coffeesConfig>,
     ) {
-        const coffeesConfig = this.configService.get('coffees.foo');
-        console.log(`database host: ${coffeesConfig}`);
+        console.log(`database host: ${coffeesConfiguration.foo}`);
     }
 
     /**
