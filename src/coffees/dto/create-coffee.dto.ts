cd16da85b985418c283137dd0cc306f7addf8b90
Arie Fitriadin <arie.fitriadin@larisin.id>
2020-12-06 23:33:59 +0700

    Decorating Model Properties

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,12 +1,16 @@
 import {IsString} from "class-validator";
+import {ApiProperty} from "@nestjs/swagger";
 
 export class CreateCoffeeDto {
+    @ApiProperty({ description: 'The Name of a coffee'})
     @IsString()
     readonly name: string;
 
+    @ApiProperty({ description: 'The Brand of the coffee'})
     @IsString()
     readonly brand: string;
 
+    @ApiProperty({ example: []})
     @IsString({each: true})
     readonly flavors: string[];
 }
